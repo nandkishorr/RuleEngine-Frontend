384fe13a42a213c36a01c534c66c4170584f7008
nandkishorr <rachulr7@gmail.com>
2024-10-26 11:00:11 +0530

    rule engine repo initialized

---
+++ rule_engine/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <p className=''>Rule Engine</p>
+    </div>
+  )
+}
+
+export default App
---
+++ rule_engine/tailwind.config.js
@@ -0,0 +1,11 @@
+// const withMT = require("@material-tailwind/react/utils/withMT");
+// import tailwindScrollbar from 'tailwind-scrollbar';
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    // tailwindScrollbar({ nocompatible: true }),
+  ],
+}
